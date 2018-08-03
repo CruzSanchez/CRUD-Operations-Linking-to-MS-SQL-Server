39e9a47f70f6ebf1dc06d2482fcabaef39855fd8
CruzSanchez <Cruzsanchez9209@gmail.com>
2018-08-03 08:29:40 -0500

    Add project files.

---
+++ CRUDUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CRUDUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
