34c5e4f88952fd5a7362f2107db2c5060497bf2d
CruzSanchez <Cruzsanchez9209@gmail.com>
2018-08-15 10:43:33 -0500

    Create, Read, and Update functions working, Delete needs attention, Added JSON Debug and Release files, Added Product and ProductRepo Classes

---
+++ CRUDUI/ProductRepo.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace CRUDUI
+{
+	public class ProductRepo
+	{
+
+		public string ConnectionString { get; set; }
+
+
+		public ProductRepo(string connectionString)
+		{
+			ConnectionString = connectionString;
+		}
+
+
+
+		public void Insert(Product product)
+		{
+			
+
+			MySqlConnection conn = new MySqlConnection(ConnectionString);
+
+			using (conn)
+			{
+				conn.Open();
+
+				MySqlCommand cmd = conn.CreateCommand();
+				cmd.CommandText = "Insert Into products (Name, Price) Values (@Name, @Price);";
+				cmd.Parameters.AddWithValue("name", product.Name);
+				cmd.Parameters.AddWithValue("price", product.Price);
+
+				cmd.ExecuteNonQuery();
+
+			}
+
+		}
+
+		public List<Product> Select()
+		{
+			
+
+			MySqlConnection conn = new MySqlConnection(ConnectionString);
+
+			using (conn)
+			{
+				conn.Open();
+
+				MySqlCommand cmd = conn.CreateCommand();
+
+				cmd.CommandText = "Select ProductID, Name, Price" +
+								  " From Products;";
+
+				MySqlDataReader dr = cmd.ExecuteReader();
+
+				List<Product> products = new List<Product>();
+
+				while (dr.Read())
+				{
+					Product product = new Product();
+					product.Id = Convert.ToInt32(dr["ProductID"]);
+					product.Name = dr["Name"].ToString();
+					product.Price = Convert.ToDecimal(dr["Price"]);
+
+					products.Add(product);
+				}
+				return products;
+			}
+
+
+		}
+
+		public void Update(Product product)
+		{
+			
+			MySqlConnection conn = new MySqlConnection(ConnectionString);
+
+			using (conn)
+			{
+				conn.Open();
+
+				MySqlCommand cmd = conn.CreateCommand();
+				cmd.CommandText = "Update Products " +
+					"Set Name = @name, Price = @price " +
+					" Where ProductID = @id; ";
+				cmd.Parameters.AddWithValue("name", product.Name);
+				cmd.Parameters.AddWithValue("price", product.Price);
+				cmd.Parameters.AddWithValue("id", product.Id);
+
+				cmd.ExecuteNonQuery();
+			}
+		}
+
+		public void Delete()
+		{
+			
+
+			MySqlConnection conn = new MySqlConnection(ConnectionString);
+
+			using (conn)
+			{
+				conn.Open();
+
+				MySqlCommand cmd = conn.CreateCommand();
+				cmd.CommandText = "Delete Name From Categories Where categoryID = 3;";
+
+				cmd.ExecuteNonQuery();
+			}
+		}
+	}
+}
--- CRUDUI/Program.cs
+++ CRUDUI/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace CRUDUI
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+		{
+
+			ProductRepo repo = new ProductRepo("Server=127.0.0.1;Database=bestbuy;Uid=root;Pwd=***********;SslMode=None;");
+
+			//List<Product> products = repo.Select();
+			//foreach(Product product in products)
+			//{
+			//	Console.WriteLine(product);
+			//}
+
+			//Console.ReadLine();
+
+			//Product prodtoinsert = new Product();
+			//prodtoinsert.Name = "Cruz Product";
+			//prodtoinsert.Price = 1000;
+
+			//repo.Insert(prodtoinsert);
+
+			//Console.WriteLine("Updated!");
+
+
+			Product productToUpdate = new Product();
+			productToUpdate.Id = 26;
+			productToUpdate.Name = "Cruz update";
+			productToUpdate.Price = 20000;
+
+			repo.Update(productToUpdate);
+
+			Console.WriteLine("UD");
+
+
+
+			Product prodDelete = new Product();
+			prodDelete.Name = "Cruz product";
+			prodDelete.Id = 26;
+
+			repo.Delete(prodDelete);
+
+			Console.ReadLine();
+
         }
+
     }
 }
